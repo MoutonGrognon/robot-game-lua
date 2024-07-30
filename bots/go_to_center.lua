8b233c4604505cc07e0eeaa50e5e2a371ea1b6a3
MoutonGrognon <compte.git@proton.me>
2024-07-30 20:36:32 +0200

    refactor: remove logs in go_to_center.lua

--- bots/go_to_center.lua
+++ bots/go_to_center.lua
@@ -1,7 +1,5 @@
 function act(self, game)
-    print(self.location)
     locs = rg.locs_around(self.location)
-    print(locs)
     loc = rg.toward(self.location, rg.CENTER_POINT)
     if (game[loc] ~= nil) then
         return { actionType=GUARD } 
