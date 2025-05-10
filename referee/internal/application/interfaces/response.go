e912753c8c0e9b18b1bdf9daf62831b86f312fb7
MoutonGrognon <compte.git@proton.me>
2025-05-10 13:33:37 +0200

    refactor(referee): remove unused interface

--- referee/internal/application/interfaces/response.go
+++ referee/internal/application/interfaces/response.go
@@ -2,10 +2,6 @@ package interfaces
 
 import "github.com/google/uuid"
 
-type StartResponse struct {
-	Started bool `json:"started"`
-}
-
 type StopResponse struct {
 	MatchId uuid.UUID `json:"matchId"`
 }
