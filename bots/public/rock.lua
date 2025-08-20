6ac3db9a4ad6cdaf3b549dedd97417f586bc5d9a
MoutonGrognon <compte.git@proton.me>
2025-08-20 19:28:40 +0200

    balance rock-paper-scissors

--- bots/public/rock.lua
+++ bots/public/rock.lua
@@ -1,8 +1,31 @@
 function act(self, game)
     target = rg.toward(self.location, rg.CENTER_POINT)
-    bot = game[target]
-    if target == self.location or (bot ~= nil) then
+    if target == self.location then
         return { actionType=GUARD } 
     end
-	return { actionType=MOVE, x=target.x, y=target.y } 
+    bot = game[target]
+    if bot == nil then 
+        return { actionType=MOVE, x=target.x, y=target.y }
+    end
+    if bot.player_id ~= self.player_id then 
+        if self.hp <= rg.SETTINGS.attack_damage.max + rg.SETTINGS.collision_damage then
+            return { actionType=SUICIDE } 
+        end
+    else
+        for _, loc in pairs(rg.locs_around(self.location)) do
+            bot = game.robots[loc]
+            if bot ~= nil and bot.player_id ~= self.player_id then
+                return { actionType=ATTACK, x=loc.x, y=loc.y }
+            end
+        end
+        for _, loc in pairs(rg.locs_around(self.location, { OBSTACLE })) do
+            for _, loc2 in pairs(rg.locs_around(loc)) do
+                bot = game.robots[loc2]
+                if bot ~= nil and bot.player_id ~= self.player_id then
+                    return { actionType=ATTACK, x=loc.x, y=loc.y }
+                end
+            end
+        end
+    end
+    return { actionType=GUARD }
 end
--- bots/public/scissors.lua
+++ bots/public/scissors.lua
@@ -1,28 +1,20 @@
 function act(self, game)
-    enemyDamage = 0
-    for _, bot in pairs(rg.locs_around(self.location)) do
-        if bot.player_id ~= self.player_id then
-            enemyDamage = enemyDamage + rg.SETTINGS.attack_damage.min
-        end
-    end
-    if self.hp <= enemyDamage or self.hp <= rg.SETTINGS.attack_damage.min then
-        return { actionType=SUICIDE }
-    end
     toCenter = rg.toward(self.location, rg.CENTER_POINT)
-    shortestDist = rg.GRID_SIZE * 2
+    shortestDist = rg.GRID_SIZE * 4
     target = toCenter
-    for _, bot in pairs(game.robots) do
+    for loc, bot in pairs(game.robots) do
         if bot.player_id ~= self.player_id then
-            loc = rg.toward(bot.location, rg.CENTER_POINT)
-            if loc == self.location then
-                return { actionType=ATTACK, x=bot.location.x, y=bot.location.y }
-            end
-            dist = rg.wdist(self.location, loc)
+            dist = rg.wdist(self.location, loc) + rg.wdist(loc, rg.CENTER_POINT)
             if dist < shortestDist then
                 shortestDist = dist
-                target = rg.toward(self.location, loc)
+                target = loc
             end
         end
     end
-    return { actionType=MOVE, x=target.x, y=target.y }
+    destination = rg.toward(target, rg.CENTER_POINT)
+    if destination == self.location then
+        return { actionType=ATTACK, x=target.x, y=target.y }
+    end
+    next_step = rg.toward(self.location, destination)
+    return { actionType=MOVE, x=next_step.x, y=next_step.y }
 end
